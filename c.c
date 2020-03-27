13d463d6dbcf0c0c7fe016829a3f5affb1082665
Garvit13 <Garvit13@noreply.github.com>
2020-03-27 17:07:32 +0530

    kdjsbfvjsdv

--- c.c
+++ c.c
@@ -2,13 +2,21 @@
 
 int main(int argc, char const *argv[])
 {
-    int a, b;
-    printf("Enter the value of A \n");
+    printf("\a");
+    int a;
+    printf("Enter the number : ");
     scanf("%d", &a);
+    printf("Table of ");
+    printf("%d", a);
+    printf(" is : ");
+    for (size_t i = 0; i < 11; i++)
+    {
+        printf("\n%d", a);
+        printf(" X ");
+        printf("%d", i);
+        printf(" = ");
+        printf("%d", a * (int)i);
+    }
 
-    printf("Enter the value of B \n");
-    scanf("%d", &b);
-
-    printf("The sum is %d\n", a + b);
     return 0;
 }
