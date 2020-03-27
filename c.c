e372d572f525da5054c98945b29a1eea9dbd6abd
Garvit13 <Garvit13@noreply.github.com>
2020-03-27 14:23:33 +0530

    sdacbsd

--- c.c
+++ c.c
@@ -2,6 +2,13 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hello");
+    int a, b;
+    printf("Enter the value of A \n");
+    scanf("%d", &a);
+
+    printf("Enter the value of B \n");
+    scanf("%d", &b);
+
+    printf("The sum is %d\n", a + b);
     return 0;
 }
