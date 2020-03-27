5f16f67e958cdeac1b82808ece461e18b9451fb4
Garvit13 <Garvit13@noreply.github.com>
2020-03-27 13:14:53 +0530

    Stated C

--- garvit2.py
+++ garvit2.py
@@ -1,12 +1,7 @@
-name = input(' Please type in your name :')
-age = input(" Please type in your age :")
-name = name.replace(' ', '')
-age1 = age.replace(' ', '')
-age1 = int(age1)
-str0 = 'You can watch coco!!'
-str1 = 'Sorry you can\'t watch coco!!'
-str2 = str0.center(len(str0) + 10, '*')
-str3 = str1.center(len(str1) + 10, '*')
-if age1 >= 10 and (name[0]=='a' or name[0]=='A'): 
-    print(str2)
-else: print(str3)
+def fibo(n):
+    if n < 2:
+        return n-1
+    return ((n-1)+(n-2))
+
+
+print(fibo(4))
