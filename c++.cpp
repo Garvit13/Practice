74c50d53e2a905bcd3b03c385e41a513796b1dff
Garvit13 <Garvit13@noreply.github.com>
2020-03-27 14:02:47 +0530

    sjdfhebjdv

--- c++.cpp
+++ c++.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class Point
 {
 	int x, y;
+	friend void distance(Point o1, Point o2);
 
 public:
 	Point(int a, int b)
@@ -18,12 +20,21 @@ public:
 	}
 };
 
+void distance(Point o1, Point o2)
+{
+	int var1 = o2.x - o1.x;
+	int var2 = o2.y - o1.y;
+	int var3 = var1 ^ 2;
+	int var4 = var2 ^ 2;
+	int var5 = var3 + var4;
+	cout << sqrt(var5) << endl;
+}
 int main()
 {
-	Point p(1, 1);
+	Point p(4, 3);
 	p.displayPoint();
-	cout << "Hello Garvit Sharma." << endl;
-	Point q(4, 6);
+	Point q(3, -2);
 	q.displayPoint();
+	distance(p, q);
 	return 0;
 }
