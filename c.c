9be723301c62b0278a7872178d051b56bfa5ee1f
Garvit13 <Garvit13@noreply.github.com>
2020-03-28 18:51:11 +0530

    sahdfjsdhvcs

--- c.c
+++ c.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-
-int main(int argc, char const *argv[])
+int main()
 {
-    printf("\a");
-    int a;
-    printf("Enter the number : ");
-    scanf("%d", &a);
-    printf("Table of ");
-    printf("%d", a);
-    printf(" is : ");
-    for (size_t i = 0; i < 11; i++)
-    {
-        printf("\n%d", a);
-        printf(" X ");
-        printf("%d", i);
-        printf(" = ");
-        printf("%d", a * (int)i);
-    }
+    int qty, dis = 0;
+    float rate = 0, total = 0;
+    printf("\n\n-----------> This Program is only check the IF statement <--------------\n\n");
 
+    printf("\n--->Please Enter the Rate of the Product: ");
+    scanf("%f", &rate);
+
+    printf("\n   If your Quantity is More then 1000 you will get 10%% Discount on Total Price.\n");
+
+    printf("\n--->Please Enter the Quantity of the Product: ");
+    scanf("%d", &qty);
+
+    if (qty > 1000)
+    {
+        dis = 10;
+    }
+    total = (qty * rate) - (qty * rate * dis / 100);
+    printf("\n  You Pay %0.2f Rs. for %d Quantity of your product", total, qty);
     return 0;
 }
