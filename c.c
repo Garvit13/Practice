1a728f530787b94e18e1ff2ffaa92831bdc4dec1
Garvit13 <Garvit13@noreply.github.com>
2020-03-29 05:56:17 -0700

    asfjcndafrecheushef ue9f  e8f  eu f

--- c.c
+++ c.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
+
+int factorial(int n)
+{
+    if (n < 2)
+    {
+        return 1;
+    }
+    return (n * factorial(n - 1));
+}
 int main()
 {
-    int qty, dis = 0;
-    float rate = 0, total = 0;
-    printf("\n\n-----------> This Program is only check the IF statement <--------------\n\n");
-
-    printf("\n--->Please Enter the Rate of the Product: ");
-    scanf("%f", &rate);
-
-    printf("\n   If your Quantity is More then 1000 you will get 10%% Discount on Total Price.\n");
-
-    printf("\n--->Please Enter the Quantity of the Product: ");
-    scanf("%d", &qty);
-
-    if (qty > 1000)
-    {
-        dis = 10;
-    }
-    total = (qty * rate) - (qty * rate * dis / 100);
-    printf("\n  You Pay %0.2f Rs. for %d Quantity of your product", total, qty);
+    int num;
+    printf("Enter the number you have to find the factorial of : \n");
+    scanf("%d", &num);
+    printf("The factorial of %d is %d \n", num, factorial(num));
     return 0;
 }
